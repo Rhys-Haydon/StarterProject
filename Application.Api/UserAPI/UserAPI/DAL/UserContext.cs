5ef4bdea4fc1847ebdc2b0f13107222671abe7d5
RhysHaydon <rhys.haydon@hotmail.com>
2018-01-30 09:16:31 +0930

    Finished adding User Initializer data

--- Application.Api/UserAPI/UserAPI/DAL/UserContext.cs
+++ Application.Api/UserAPI/UserAPI/DAL/UserContext.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
+using UserAPI.Models;
 
 namespace UserAPI.DAL
 {
     public class UserContext : DbContext
     {
 
+        public UserContext() : base("UserContext") {
+        }
 
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
--- Application.Api/UserAPI/UserAPI/DAL/UserInitializer.cs
+++ Application.Api/UserAPI/UserAPI/DAL/UserInitializer.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using UserAPI.Models;
 
 namespace UserAPI.DAL
 {
-    public class UserInitializer
+    public class UserInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<UserContext>
     {
+        protected override void Seed(UserContext context)
+        {
+            var users = new List<User>
+            {
+                new User{FirstName="Bat",LastName="Man"},
+                new User{FirstName="Killer",LastName="Croc"},
+                new User{FirstName="John",LastName="Smith"},
+                new User{FirstName="Agent",LastName="Smith"},
+                new User{FirstName="Smith",LastName="Wesson"}
+            };
+
+            users.ForEach(s => context.Users.Add(s));
+            context.SaveChanges();
+        }
     }
 }
